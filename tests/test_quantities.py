b561633fbaf32a48dcfb18fc9cad8a79631e98f0
Sahas Subramanian <sahas.subramanian@proton.me>
2025-06-17 16:26:58 +0200

    Remove the special cached zero value
    This will no-longer be easy once we support decimals as the base
    value.
    
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- tests/test_quantities.py
+++ tests/test_quantities.py
@@ -93,87 +93,10 @@ _QUANTITY_CTORS = [
 assert len(_QUANTITY_CTORS) >= _SANITFY_NUM_CLASSES
 
 
-def test_zero() -> None:  # pylint: disable=too-many-statements
-    """Test the zero value for quantity."""
-    assert Quantity(0.0) == Quantity.zero()
-    assert Quantity(0.0, exponent=100) == Quantity.zero()
-    assert Quantity.zero() is Quantity.zero()  # It is a "singleton"
-    assert Quantity.zero().base_value == 0.0
-
-    # Test the singleton is immutable
-    one = Quantity.zero()
-    one += Quantity(1.0)
-    assert one != Quantity.zero()
-    assert Quantity.zero() == Quantity(0.0)
-
-    assert Power.from_watts(0.0) == Power.zero()
-    assert Power.from_kilowatts(0.0) == Power.zero()
-    assert isinstance(Power.zero(), Power)
-    assert Power.zero().as_watts() == 0.0
-    assert Power.zero().as_kilowatts() == 0.0
-    assert Power.zero() is Power.zero()  # It is a "singleton"
-
-    assert ReactivePower.from_volt_amperes_reactive(0.0) == ReactivePower.zero()
-    assert ReactivePower.from_kilo_volt_amperes_reactive(0.0) == ReactivePower.zero()
-    assert isinstance(ReactivePower.zero(), ReactivePower)
-    assert ReactivePower.zero().as_volt_amperes_reactive() == 0.0
-    assert ReactivePower.zero().as_kilo_volt_amperes_reactive() == 0.0
-    assert ReactivePower.zero() is ReactivePower.zero()  # It is a "singleton"
-
-    assert ApparentPower.from_volt_amperes(0.0) == ApparentPower.zero()
-    assert ApparentPower.from_kilo_volt_amperes(0.0) == ApparentPower.zero()
-    assert isinstance(ApparentPower.zero(), ApparentPower)
-    assert ApparentPower.zero().as_volt_amperes() == 0.0
-    assert ApparentPower.zero().as_kilo_volt_amperes() == 0.0
-    assert ApparentPower.zero() is ApparentPower.zero()  # It is a "singleton"
-
-    assert Current.from_amperes(0.0) == Current.zero()
-    assert Current.from_milliamperes(0.0) == Current.zero()
-    assert isinstance(Current.zero(), Current)
-    assert Current.zero().as_amperes() == 0.0
-    assert Current.zero().as_milliamperes() == 0.0
-    assert Current.zero() is Current.zero()  # It is a "singleton"
-
-    assert Voltage.from_volts(0.0) == Voltage.zero()
-    assert Voltage.from_kilovolts(0.0) == Voltage.zero()
-    assert isinstance(Voltage.zero(), Voltage)
-    assert Voltage.zero().as_volts() == 0.0
-    assert Voltage.zero().as_kilovolts() == 0.0
-    assert Voltage.zero() is Voltage.zero()  # It is a "singleton"
-
-    assert Energy.from_kilowatt_hours(0.0) == Energy.zero()
-    assert Energy.from_megawatt_hours(0.0) == Energy.zero()
-    assert isinstance(Energy.zero(), Energy)
-    assert Energy.zero().as_kilowatt_hours() == 0.0
-    assert Energy.zero().as_megawatt_hours() == 0.0
-    assert Energy.zero() is Energy.zero()  # It is a "singleton"
-
-    assert Frequency.from_hertz(0.0) == Frequency.zero()
-    assert Frequency.from_megahertz(0.0) == Frequency.zero()
-    assert isinstance(Frequency.zero(), Frequency)
-    assert Frequency.zero().as_hertz() == 0.0
-    assert Frequency.zero().as_megahertz() == 0.0
-    assert Frequency.zero() is Frequency.zero()  # It is a "singleton"
-
-    assert Percentage.from_percent(0.0) == Percentage.zero()
-    assert Percentage.from_fraction(0.0) == Percentage.zero()
-    assert isinstance(Percentage.zero(), Percentage)
-    assert Percentage.zero().as_percent() == 0.0
-    assert Percentage.zero().as_fraction() == 0.0
-    assert Percentage.zero() is Percentage.zero()  # It is a "singleton"
-
-
 @pytest.mark.parametrize("quantity_ctor", _QUANTITY_CTORS)
 def test_base_value_from_ctor_is_float(quantity_ctor: _CtorType) -> None:
     """Test that the base value always is a float."""
     quantity = quantity_ctor(1)
-    assert isinstance(quantity.base_value, float)
-
-
-@pytest.mark.parametrize("quantity_type", _QUANTITY_SUBCLASSES + [Quantity])
-def test_base_value_from_zero_is_float(quantity_type: type[Quantity]) -> None:
-    """Test that the base value always is a float."""
-    quantity = quantity_type.zero()
     assert isinstance(quantity.base_value, float)
 
 
